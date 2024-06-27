c28d0ee528ac2dadeafede0690ff5b4336dd6e35
Ogban Ugot <ogbanugot>
2024-06-27 10:01:33 +0100

    add hf dataset

---
+++ hf_dataset.py
@@ -0,0 +1,22 @@
+import csv
+import os
+import random
+from tqdm import tqdm
+from create_metadata import dataset_path
+from datasets import DatasetDict
+
+dset = tqdm(os.listdir(dataset_path))
+
+headers = ['audio']
+with open('dataset.csv', 'w', newline='') as file:
+    writer = csv.writer(file)
+    writer.writerow(headers)
+    for filename in dset:
+      if filename.endswith(('.mp3', '.wav', '.flac')):
+        path = os.path.join(dataset_path, filename)
+        result = [path]
+        writer.writerow(result)
+
+dataset = DatasetDict.from_csv({"train": "dataset.csv"})
+dataset = dataset.cast_column("audio", Audio())
+dataset.save_to_disk("dataset/train")
