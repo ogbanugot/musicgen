7610223924c2717d7a5ad57b05a2d0fab3404f90
Ogban Ugot <ogbanugot>
2024-06-30 12:23:39 +0100

    modify audiolm inference

--- audiolm_inference.py
+++ audiolm_inference.py
@@ -1,5 +1,14 @@
 import torch
-from audiolm import semantic_transformer
+from audiolm_pytorch import SemanticTransformer
+
+semantic_transformer = SemanticTransformer(
+    num_semantic_tokens=500,
+    dim=1024,
+    depth=6,
+    has_condition=True,  # this will have to be set to True
+    cond_as_self_attn_prefix=True
+    # whether to condition as prefix to self attention, instead of cross attention, as was done in 'VALL-E' paper
+).cuda()
 
 
 def load_model_for_inference(model, checkpoint_path='model_checkpoint.pth'):
@@ -9,7 +18,7 @@ def load_model_for_inference(model, checkpoint_path='model_checkpoint.pth'):
 
 
 model_for_inference = semantic_transformer
-model = load_model_for_inference(model_for_inference, 'model_checkpoint.pth')
+model = load_model_for_inference(model_for_inference, 'audiolm_checkpoint.pth')
 
 sample = model.generate(text=['sound of rain drops on the rooftops'], batch_size=1,
                         max_length=2)  # (1, < 128) - may terminate early if it detects [eos]
