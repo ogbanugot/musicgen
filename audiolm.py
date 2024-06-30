904325208c1506e3a57e5c112ea451de3593468b
Ogban Ugot <ogbanugot>
2024-06-30 21:03:53 +0100

    reduce batch size

--- audiolm.py
+++ audiolm.py
@@ -122,7 +122,7 @@ def train_fine():
         transformer=fine_transformer,
         codec=encodec,
         dataset=TextAudioDataset(),
-        batch_size=2,
+        batch_size=1,
         data_max_length=30,
         valid_frac=0.1,
         save_model_every=training_steps - 1,
