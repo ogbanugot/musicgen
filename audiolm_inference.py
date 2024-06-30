20f0aaf35738ca397fc4c48c6e1cd5aada729476
Ogban Ugot <ogbanugot>
2024-06-30 13:20:07 +0100

    modify audiolm inference

--- audiolm_inference.py
+++ audiolm_inference.py
@@ -22,5 +22,5 @@ model = load_model_for_inference(model_for_inference, 'audiolm_checkpoint.pth')
 sample = model.generate(text=['sound of rain drops on the rooftops'], batch_size=1,
                         max_length=128)  # (1, < 128) - may terminate early if it detects [eos]
 
-audio_values = sample[0].detach().cpu().numpy()
+audio_values = sample[0].detach().cpu().float().numpy()
 sf.write("audiolm_out_0.wav", audio_values, 44100)
