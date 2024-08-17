d6cb096de6d51b47a59245107098749cd7a79fa8
Ogban Ugot <ogbanugot>
2024-08-17 08:30:44 +0100

    concurrent separate

--- separate.py
+++ separate.py
@@ -1,10 +1,22 @@
 import os
 import shutil
 from tqdm import tqdm
+from concurrent.futures import ThreadPoolExecutor
 from audio_separator.separator import Separator
 
 dataset_path = "/home/pythonuser/project/data"
 vocals_path = "/home/pythonuser/project/vocals"
+
+
+def process_file(filename, separator, vocals_path):
+    if filename.endswith(('.mp3', '.wav', '.flac')):
+        filename = os.path.join(dataset_path, filename)
+
+        # Perform the separation on specific audio files without reloading the model
+        output_files = separator.separate(filename)
+
+        print(f"Separation complete! Output file(s): {' '.join(output_files)}")
+        shutil.copy(output_files[1], os.path.join(vocals_path, os.path.basename(output_files[1])))
 
 
 def separate_vocals():
@@ -15,16 +27,13 @@ def separate_vocals():
 
     # Load a machine learning model (if unspecified, defaults to 'model_mel_band_roformer_ep_3005_sdr_11.4360.ckpt')
     separator.load_model()
-    prg = tqdm(os.listdir(dataset_path))
-    for filename in prg:
-        if filename.endswith(('.mp3', '.wav', '.flac')):
-            filename = os.path.join(dataset_path, filename)
 
-            # Perform the separation on specific audio files without reloading the model
-            output_files = separator.separate(filename)
+    # Get the list of files to process
+    files = [f for f in os.listdir(dataset_path) if f.endswith(('.mp3', '.wav', '.flac'))]
 
-            print(f"Separation complete! Output file(s): {' '.join(output_files)}")
-            shutil.copy(output_files[1], os.path.join(vocals_path, output_files[1]))
+    # Use ThreadPoolExecutor to parallelize the file processing
+    with ThreadPoolExecutor() as executor:
+        list(tqdm(executor.map(lambda f: process_file(f, separator, vocals_path), files), total=len(files)))
 
 
 if __name__ == '__main__':
